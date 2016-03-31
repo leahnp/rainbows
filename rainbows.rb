35834fc2b870e6d8a90601e54d3d0aa5eca06bcb
Leah <leahnpetersen@gmail.com>
2016-03-30 20:32:48 -0700

    added print_rainbow method and rainbow array of hashes:

--- rainbows.rb
+++ rainbows.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 red = [255,0,0]
 green = [0,255,0]
 blue = [0,0,255]
@@ -17,6 +19,8 @@ color_lookup = {
   [255,0,125] => "raspberry"
 }
 
+rainbow = [{red:{r:255,g:0,b:0}}, {orange:{r:255,g:127,b:0}}, {yellow:{r:255,g:255,b:0}}, {green:{r:0,g:255,b:0}}, {blue:{r:0,g:0,b:255}}, {violet:{r:75,g:0,b:130}}, {ultra_violet:{r:143,g:0,b:255}}]
+
 def mix_colors(color1,color2)
   mixed_color = []
   3.times do |i|
@@ -25,5 +29,16 @@ def mix_colors(color1,color2)
   return mixed_color
 end
 
-new_color = mix_colors(red,green)
-puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lookup[new_color]} #{new_color}"
+def print_rainbow(array)
+  array.each do |element|
+    color = element.keys
+    color_string = color[0].to_s
+    puts color_string.colorize(color[0])
+  end
+end
+
+print_rainbow(rainbow)
+
+
+# new_color = mix_colors(red,green)
+# puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lookup[new_color]} #{new_color}"
